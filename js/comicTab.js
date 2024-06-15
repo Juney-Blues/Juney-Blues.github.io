a0a256bf4de7251b2a89e263d811b9853ac963f2
Juney-Blues <137285567+Juney-Blues@users.noreply.github.com>
2024-06-15 03:20:00 +0100

    Add files via upload

---
+++ js/comicTab.js
@@ -0,0 +1,48 @@
+var comicTabs = [
+	{
+		"title": `WATCH AS I PERFORM MY OWN TRACHEOTOMY (Working Title)`,
+		"description":`
+		<p>originally hosted on <a href="https://juney-blues.tumblr.com/tracheotomy">TUMBLR</a></p>
+		<p>A girl dissociates in her apartment and has a generally normal time</p>
+		<p><b id="tracheotomyLength"></b> pages and counting!</p>
+		<p><a href="tracheotomy">READ IT HERE.</a>`,
+	},
+	{
+		"title": "Placeholder!",
+		"description":`
+		<p>I'll put something cool here later!</p>
+		<p>I swear!</p>`,
+	},
+	{
+		"title": "Placeholder2!",
+		"description":`<p>blugh!</p>`,
+	},
+]
+
+
+
+let currentComicTab = 0;
+
+
+goToTab(0);
+
+
+function goToTab(tabIndex) {
+  currentComicTab = tabIndex;
+
+  document.getElementById("comicTitle").innerHTML = `${comicTabs[currentComicTab].title}`;
+  document.getElementById("comicDescription").innerHTML = `${comicTabs[currentComicTab].description}`;
+  /*tracheotomy length*/
+  if (document.getElementById("tracheotomyLength")) {
+  document.getElementById("tracheotomyLength").innerHTML = pageList.length - 1;
+  } 
+  
+    // VRiska virksa virksa
+	comicarr = document.querySelectorAll(".comictab")
+	for (let i = 0; i < comicarr.length; i++) {
+    comicarr[i].className = "comictab low"
+	}
+	if (comicarr[tabIndex + 1]) comicarr[tabIndex + 1].className = "comictab medium"
+	if (comicarr[tabIndex - 1]) comicarr[tabIndex - 1].className = "comictab medium"
+	comicarr[tabIndex].className = "comictab high"
+}
