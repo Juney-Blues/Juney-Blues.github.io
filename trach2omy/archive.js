7d4d89eeafd462e3acd99fecf54fbe16e97dfb56
Juney-Blues <junebugwritesstuff@gmail.com>
2024-08-24 05:46:29 +0100

    trach2omy archive now fully works, other stuff also

--- trach2omy/archive.js
+++ trach2omy/archive.js
@@ -14,16 +14,34 @@ const loadArchive = async () => {
 	const archiveList = comicObject.archive //.sort((a,b) => a.date > b.date)
 	const chapterList = comicObject.chapters
 	
-	//variables i'm not defining yet
-	let chapterDateStart,chapterDateEnd = " " //TODO, GET DATES SOMEHOW
-
+	
+	//combine list (keeping track of the proper index for each page so it doesn't get messed up by sorting)
+	//==============================================================================
+	for (let i = 0; i < pageList.length; i++) {
+		pageList[i].pgNum = i;
+	}
+	const combinedList = pageList.concat(archiveList).sort((a,b) => a.date > b.date)
+	//==============================================================================
 	//add chapters to webpage 
 	//==========================================================================================================
 	for (let i = 0; i < chapterList.length; i++) {
 		let chapter = chapterList[i]
-			
-		document.querySelector("#main").insertAdjacentHTML("afterbegin",`
-		<details class="archivecontainer">
+		let nextChapter = chapterList[i+1]
+		//get last page, this is a little messier than i'd like, feel free to clean it up if you want
+		//=======================================
+		if(i < chapterList.length -1){
+			lastPage = nextChapter.firstPage - 1;
+		}
+		else{
+			lastPage = pageList.length -1;
+		}
+		//=======================================
+		//get dates
+		let chapterDateStart = new Date(pageList[chapter.firstPage].date).toLocaleDateString()
+		let chapterDateEnd = new Date(pageList[lastPage].date).toLocaleDateString()
+		
+		document.querySelector("#archiveContainer").insertAdjacentHTML("afterbegin",`
+		<details class="archive-chapter">
 			<summary class="archive-header collapsible" id="chapter${i}">
 				<B>${chapter.title1}</B><br>${chapter.title2}<br><i>${chapterDateStart} - ${chapterDateEnd}</i>
 			</summary>
@@ -32,21 +50,21 @@ const loadArchive = async () => {
 		`)	
 	}
 	//==========================================================================================================
-	//add pages to chapters
+	//add pages/archive entries to chapters
 	//==============================================================================================================================================================
-	for (let i = 0; i < pageList.length; i++) {
-		let page = pageList[i]
+	for (let i = 0; i < combinedList.length; i++) {
+		let item = combinedList[i]
 		
-		let pageDate = new Date(page.date).toLocaleDateString();
-		let pageTitle = page.title ? page.title : i ? `Page ${i}` : "Update" //flare wrote this line and it is dark javascript magic as far is i'm concerned - june
-		let pageLink = `../?pg=${i}`
+		let itemDate = new Date(item.date).toLocaleDateString()
+		let itemTitle = item.title ? item.title : item.pgNum ? `Page ${item.pgNum}` : "Update" //flare wrote this line and it is dark javascript magic as far is i'm concerned - june
+		let itemLink = item.url ? item.url : item.pgNum ? `../?pg=${item.pgNum}` : " " 
 		
-		let pageChapter = chapterList.findLastIndex(x => x.firstPage <= i);
-		console.log(pageChapter)
-		
-		document.querySelector(`#list${pageChapter}`).insertAdjacentHTML("afterbegin", `<li><b>${pageDate}</b> - <a href="${pageLink}">${pageTitle}</a></li>`)
+		let itemChapter = chapterList.findLastIndex(x => x.firstPage <= i)		
+
+		document.querySelector(`#list${itemChapter}`).insertAdjacentHTML("afterbegin", `<li><b>${itemDate}</b> - <a href="${itemLink}">${itemTitle}</a></li>`)
 	}
 	//==============================================================================================================================================================
+	
 	
 	//make most recent chapter open by default
 	document.getElementsByTagName("details")[0].setAttribute("open","");
