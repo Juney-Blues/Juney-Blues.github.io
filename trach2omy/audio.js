9317e57ea28ee98f7c594efd939c71b5c7b24abc
FlaringK <caelans@iinet.net.au>
2025-08-17 13:09:22 +0800

    Fixed audio

--- trach2omy/audio.js
+++ trach2omy/audio.js
@@ -1,81 +1,95 @@
-
+// Ambience audio
 let backgroundAmbience1 = document.getElementById("backgroundAmbience1")
 let backgroundAmbience2 = document.getElementById("backgroundAmbience2")
-
-//set volumes to 0 so we can fade them in
 backgroundAmbience1.volume = 0;
 backgroundAmbience2.volume = 0;
-//having 2 audio tracks so we can crossfade between them
-let activeAmbient = backgroundAmbience1;
-let activeMusic = backgroundMusic1;
 
-let pageMusic    = ""
-let pageAmbience = ""
+let audioElements = [backgroundAmbience1, backgroundAmbience2]
 
-let fadeTime = 1;
-let fadeInVolume = 1;
-//set music and ambience
-const setPageSound = async (page) =>{
-	if (page.ambience == null) { 
-		pageAmbience = ""
+// Having 2 audio tracks so we can crossfade between them
+let ambActiveIndex = 0;
+let ambPageLink = ""
+let ambFadeTime = 1;
+let ambFadeInInterval;
+let ambFadeOutInterval;
+
+// Set up crossfade
+const ambCrossFadeAudio = () => {
+	// Clear intervals from previous page
+	if (ambFadeInInterval) { clearInterval(ambFadeInInterval) }
+	if (ambFadeOutInterval) { clearInterval(ambFadeOutInterval) }
+
+	// If durations are the same
+	if (getInactiveAudio().duration = getActiveAudio().duration) {
+		getActiveAudio().currentTime = getInactiveAudio().currentTime
 	}
-	else {
-		pageAmbience = comicObject.linkPrefix + page.ambience
+
+	// Fade out inactive audio
+	getInactiveAudio().volume = 1
+	ambFadeOutInterval = fadeAudioOut(getInactiveAudio(), 1, ambFadeTime)
+
+	// Fade in active audio
+	getActiveAudio.volume = 0;
+	ambFadeInInterval = fadeAudioIn(getActiveAudio(), 1, ambFadeTime)
+}
+backgroundAmbience1.onloadedmetadata = ambCrossFadeAudio
+backgroundAmbience2.onloadedmetadata = ambCrossFadeAudio
+
+// Music Audio (Currently Unused)
+// let activeMusicAudio = backgroundMusic1;
+// let pageMusicLink = ""
+
+// Set music and ambience
+const setPageSound = async (page) => {
+
+	// Fade between Ambience
+	pageAmbienceLink = page.ambience ? comicObject.linkPrefix + page.ambience : ""
+	if (pageAmbienceLink != getActiveAudio().src) {
+
+		// Switch active audio element
+		ambActiveIndex = (ambActiveIndex + 1) % 2
+
+		if (pageAmbienceLink) {
+			getActiveAudio().src = pageAmbienceLink
+		} else {
+			// Fade out inactive audio
+			getInactiveAudio().volume = 1
+			ambFadeOutInterval = fadeAudioOut(getInactiveAudio(), 1, ambFadeTime)
+		}
 	}
-	if (page.music == null) { 
-		pageMusic = ""
-	}
-	else {
-		pageMusic = comicObject.linkPrefix + page.music
-    }
-	
-    //keep ambience looping between pages without getting interrupted
-	if(pageAmbience != activeAmbient.src){
-		//fade out current audio
-		fadeAudioOut(activeAmbient,activeAmbient.volume);
-		// switch audio element to avoid gap in audio
-		activeAmbient = activeAmbient == backgroundAmbience2 ? backgroundAmbience1 : backgroundAmbience2;
-		//keep current time between ambience sounds
-		prevTime = activeAmbient.currentTime;
-		activeAmbient.src = pageAmbience;
-		activeAmbient.currentTime = prevTime;	
-		//fade in current audio
-		fadeAudioIn(activeAmbient,fadeInVolume);
-	}
-	if(pageMusic != activeMusic.src){
-		activeMusic.src = pageMusic;
-	}
+
+	// Set music (Currently Unused)
+	// pageMusicLink = page.music ? comicObject.linkPrefix + page.music : ""
+	// if (pageMusicLink != activeMusic.src) {
+	// 	activeMusic.src = pageMusicLink;
+	// }
 }
 
-//fading code courtesy of flare >:D
-
-//bwehhh these can break if you change pages fast enough that the fade intervals start interfering with themselves
-//not sure how to fix it
+let getActiveAudio = () => audioElements[ambActiveIndex]
+let getInactiveAudio = () => audioElements[(ambActiveIndex + 1) % 2]
 
 // Fade in
-let fadeAudioIn = (fadingInAudio,setVolume) => {
-  let fadeInInterval = setInterval(() => {
-    fadingInAudio.volume += setVolume / fadeTime / 10 
-	
-    // accounts for float error
-    if (activeAmbient.volume + setVolume / fadeTime / 10 >= setVolume) {
-      clearInterval(fadeInInterval)
-      activeAmbient.volume = setVolume
-    }
-  }, 100)
+let fadeAudioIn = (audioElement, setVolume, fadeTime) => {
+	let interval = setInterval(() => {
+		audioElement.volume += setVolume / fadeTime / 10
+		// Accounts for float error
+		if (audioElement.volume + setVolume / fadeTime / 10 >= setVolume) {
+			clearInterval(interval)
+			audioElement.volume = setVolume
+		}
+	}, 100)
+	return interval
 }
+
 // Fade out
-let fadeAudioOut = (fadingOutAudio,currentVolume) => {
-  let fadeOutInterval = setInterval(() => {
-    fadingOutAudio.volume -= currentVolume / fadeTime / 10 
-    // accounts for float error
-    if (fadingOutAudio.volume - currentVolume / fadeTime / 10 <= 0) {
-      clearInterval(fadeOutInterval)
-      fadingOutAudio.volume = 0
-    }
-  }, 100)
+let fadeAudioOut = (audioElement, currentVolume, fadeTime) => {
+	let interval = setInterval(() => {
+		audioElement.volume -= currentVolume / fadeTime / 10
+		// Accounts for float error
+		if (audioElement.volume - currentVolume / fadeTime / 10 <= 0) {
+			clearInterval(interval)
+			audioElement.volume = 0
+		}
+	}, 100)
+	return interval
 }
-
-
-//preload images and sounds, usually the next page's
-
--- trach2omy/tracheotomy.js
+++ trach2omy/tracheotomy.js
@@ -1,9 +1,17 @@
-
 // Usernames
 const knownUsers = {
-  yellow: {color: "#cfa630", username: "Laikaboss7008"},
-  teal: {color: "#007880", username: "SpringInsect113"},
-  none: {color: "#007880", username: ""},
+	yellow: {
+		color: "#cfa630",
+		username: "Laikaboss7008"
+	},
+	teal: {
+		color: "#007880",
+		username: "SpringInsect113"
+	},
+	none: {
+		color: "#007880",
+		username: ""
+	},
 }
 
 // LOAD COMIC OBJECT
@@ -12,105 +20,103 @@ let subPage = "/trach2omy"
 fetch('./tracheotomyComic.json').then((response) => response.json()).then((json) => initComic(json));
 
 const initComic = json => {
-  comicObject = json
-  loadPage()
+	comicObject = json
+	loadPage()
 }
 
 const getUrlPage = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const p = urlParams.get("pg")
-  return p ? p : "0"
+	const urlParams = new URLSearchParams(window.location.search);
+	const p = urlParams.get("pg")
+	return p ? p : "0"
 }
 
 const loadPage = async () => {
-  const pageIndex = parseInt(getUrlPage())
-  //console.log(pageIndex)
-  const page = comicObject.pages[pageIndex]
-  const comicLength = comicObject.pages.length
+	const pageIndex = parseInt(getUrlPage())
+	//console.log(pageIndex)
+	const page = comicObject.pages[pageIndex]
+	const comicLength = comicObject.pages.length
 
-  // LOAD PAGE
-  if ("custom" in page) {
-    // Load custom page
-    let pageHtml = await fetch(`./customPages/${pageIndex}.html`).then(e => e.text())
-    pageHtml = pageHtml.replace("MAINIMAGE", comicObject.linkPrefix + page.img)
-    document.getElementById("pageContent").innerHTML = pageHtml 
+	// LOAD PAGE
+	if ("custom" in page) {
+		// Load custom page
+		let pageHtml = await fetch(`./customPages/${pageIndex}.html`).then(e => e.text())
+		pageHtml = pageHtml.replace("MAINIMAGE", comicObject.linkPrefix + page.img)
+		document.getElementById("pageContent").innerHTML = pageHtml
+		initCustomPage()
+	} else {
+		// Load basic page
+		let img = document.createElement("img")
+		img.src = comicObject.linkPrefix + page.img
+		document.getElementById("pageContent").innerHTML = ""
+		document.getElementById("pageContent").append(img)
+	}
 
-    initCustomPage()
-  } else {
-    // Load basic page
-    let img = document.createElement("img")
-    img.src = comicObject.linkPrefix + page.img
-    document.getElementById("pageContent").innerHTML = ""
-    document.getElementById("pageContent").append(img)
-  }
+	// Set Theme
+	document.body.className = page.theme ?? ""
 
-  // Set Theme
-  document.body.className = page.theme ?? ""
+	// Set next and prev
+	const nextPage = document.getElementById("nextPage")
+	const prevPage = document.getElementById("prevPage")
 
-  // Set next and prev
-  const nextPage = document.getElementById("nextPage")
-  const prevPage = document.getElementById("prevPage")
+	nextPage.href = subPage + "/?pg=" + (pageIndex + 1)
+	prevPage.href = subPage + "/?pg=" + (pageIndex - 1)
 
-  nextPage.href = subPage + "/?pg=" + (pageIndex + 1)
-  prevPage.href = subPage + "/?pg=" + (pageIndex - 1)
+	nextPage.style.display = pageIndex > comicLength - 2 ? "none" : "inline"
+	prevPage.style.display = pageIndex < 1 ? "none" : "inline"
 
-  nextPage.style.display = pageIndex > comicLength - 2 ? "none" : "inline"
-  prevPage.style.display = pageIndex < 1 ? "none" : "inline"
+	// Preload next pages
+	if (pageIndex < comicLength - 2) {
+		document.getElementById("preloadNext").src = comicObject.linkPrefix + comicObject.pages[pageIndex + 1].img
+	}
+	if (pageIndex > 1) {
+		document.getElementById("preloadPrev").src = comicObject.linkPrefix + comicObject.pages[pageIndex - 1].img
+	}
 
-  // Preload next pages
-  if (pageIndex < comicLength - 2) { 
-    document.getElementById("preloadNext").src = comicObject.linkPrefix + comicObject.pages[pageIndex + 1].img 
-  }
-  if (pageIndex > 1) { 
-    document.getElementById("preloadPrev").src = comicObject.linkPrefix + comicObject.pages[pageIndex - 1].img 
-  }
-  
-  // Load additional credits
-  if (page.credit != null) {
-	document.getElementById("pageCredits").innerHTML = page.credit
-  }
-  else{
-	document.getElementById("pageCredits").innerHTML = ""
-  }
+	// Load additional credits
+	if (page.credit != null) {
+		document.getElementById("pageCredits").innerHTML = page.credit
+	} else {
+		document.getElementById("pageCredits").innerHTML = ""
+	}
 
-  // Scroll to top
-  window.scrollTo(0, 0);
-  
-  //load audio
-  setPageSound(page);
+	// Scroll to top
+	window.scrollTo(0, 0);
+
+	//load audio
+	setPageSound(page);
 }
 
 // Init custom page events
 const initCustomPage = () => {
 
-  // Check for dissonancelog
-  const dissonancelogs = document.querySelectorAll(".dissonancelog")
+	// Check for dissonancelog
+	const dissonancelogs = document.querySelectorAll(".dissonancelog")
 
-  dissonancelogs.forEach(log => {
-    let prevUser = ""
+	dissonancelogs.forEach(log => {
+		let prevUser = ""
 
-    log.querySelectorAll("p").forEach(msg => {
-      let user = msg.dataset.user
-      if (prevUser != user) {
-        let knownUser = knownUsers[user]
+		log.querySelectorAll("p").forEach(msg => {
+			let user = msg.dataset.user
+			if (prevUser != user) {
+				let knownUser = knownUsers[user]
 
-        if (knownUser.username) {
-          msg.innerHTML = `<b style="color: ${knownUser.color}">${knownUser.username}:</b> <br>` + msg.innerHTML
-          msg.className = "newMsgUser"
-        }
+				if (knownUser.username) {
+					msg.innerHTML = `<b style="color: ${knownUser.color}">${knownUser.username}:</b> <br>` + msg.innerHTML
+					msg.className = "newMsgUser"
+				}
 
-      }
-      prevUser = user
-    })
-  })
+			}
+			prevUser = user
+		})
+	})
 
 }
 
 // Handle click events
 const clickLink = (event, link) => {
-  event.preventDefault()
-  history.pushState(null, '', link)
-  loadPage()
+	event.preventDefault()
+	history.pushState(null, '', link)
+	loadPage()
 }
 // reload page on popstate
 window.onpopstate = (event) => {
@@ -118,4 +124,6 @@ window.onpopstate = (event) => {
 }
 
 
-document.querySelectorAll(".pageLink").forEach(a => { a.addEventListener("click", evt => clickLink(evt, a.href)) })
+document.querySelectorAll(".pageLink").forEach(a => {
+	a.addEventListener("click", evt => clickLink(evt, a.href))
+})
